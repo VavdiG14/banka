56b0ed1c6276058935cbf1542d57b735643c8053
Erika Šavli <erika.savli@gmail.com>
2017-11-16 10:28:12 +0100

    dodana imena

--- banka.py
+++ banka.py
@@ -1,15 +1,10 @@
 # Testni projekt o banki.
 # Sodelavci na projektu:
 # - alenFMF
-<<<<<<< HEAD
-<<<<<<< HEAD
 # - SavliE12
-=======
 # - kulan89
->>>>>>> ffd841cb520d1a6d319f761d782f17ea5cddd873
-=======
 # - Larisa123
->>>>>>> 8e246fced7b10a55ed3b595bdc96806faebcfd73
+
 
 import sqlite3
 
